1e8cd9e0d55d55535d5214c3cef6f1bc58fb9c97
estefaniay9 <36626411+estefaniay9@users.noreply.github.com>
2020-09-17 00:28:39 +1000

    Create IGDplus.m

---
+++ PlatEMO v2.2.1/Metrics/IGDplus.m
@@ -0,0 +1,19 @@
+function Score = IGDplus(PopObj,PF)
+% <metric> <min>
+% Using Ishibuchi et al. 2015
+% Modified Distance Calculation in Generational Distance and Inverted
+% Generational Distance
+
+%--------------------------------------------------------------------------
+% Copyright (c) 2016-2017 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB Platform
+% for Evolutionary Multi-Objective Optimization [Educational Forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+	rmimg = find(sum(imag(PopObj)~= 0,2));
+	PopObj(rmimg,:) = [];
+	Distance = min(pdist2(PF,PopObj, @dplus),[],2);	
+    Score    = mean(Distance);
+end
