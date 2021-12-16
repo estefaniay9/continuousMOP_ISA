94fea8e694532be7d3f68f879cea3fc6d53d875f
estefaniay9 <36626411+estefaniay9@users.noreply.github.com>
2021-12-16 21:11:13 +1100

    Delete rankprop.m

--- PlatEMO v2.2.1/Features/rankprop.m
+++
@@ -1,6 +0,0 @@
-function x = rankprop(objvar,n)
-    %------------------Evolvability------------------
-    % Proportion of rank n
-    rankn = objvar(NDSort(objvar,length(objvar))==n,:);
-    x = numel(rankn)/numel(objvar);
-end
